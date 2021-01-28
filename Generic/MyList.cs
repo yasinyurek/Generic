20c062f008106aabc454fb5254ec40062cd66ff5
yasinyurek <yasinyurek.dev@gmail.com>
2021-01-28 18:25:58 +0300

    Add project files.

---
+++ Generic/MyList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic
+{
+    // list tarzı yazmak için constructor ve bazı özellikleri içeren bir class lazım.
+    class MyList<T>
+    {
+        // itemleri tutacağımız bir array abi lazım. 
+        T[] items;
+
+
+        public MyList()
+        {
+            //constructor reisde 0 elemanlı bir item arrayi yapıyoruz.
+
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            //tempArray abimize eski referans numarasını tutturuyoruz.
+            T[] tempArray = items;
+
+            //Yeni bir tane boşluk açıyoruz ki eleman alalım.
+            items = new T[items.Length + 1];
+
+            // emanetcimiz olan tempArrayden emanetleri geri alıyoruz.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            // itemsdeki boşluğa add ile eleman yerleştiriyoruz.
+            items[items.Length - 1] = item;
+
+            // burada da programdan neler ekledik ona bakalım.
+            Console.WriteLine(item);
+        }
+
+        
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // tertemiz bir generic yaptık h.o.
+            MyList<string> itemler = new MyList<string>();
+
+
+            // Add ile artık dizinize istediğimiz kadar zamazingo ekleyebiliriz.
+            itemler.Add("+9 dolunay kılıcı");
+            itemler.Add("+3 hilal kılıcı");
+            itemler.Add("+2 Sos nuker kılıcı");
+
+
+        }
+    }
+}
